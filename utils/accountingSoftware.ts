f250904942e02b5afc93fe941abe29707b812b6c
itsyashsahu <itsyashsahu@gmail.com>
2023-09-03 00:06:03 +0530

    env

--- utils/accountingSoftware.ts
+++ utils/accountingSoftware.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 const fetchBalanceSheet = async (userId: string) => {
   try {
     const url = `${process.env.PLATFORM_INTEGRATION_URL}/api/balance-sheet?userId=${userId}`;
-    const result = await axios.get(url);
     console.log("🚀 ~ fetchBalanceSheet ~ url:", url);
+    const result = await axios.get(url);
     return result.data.sheet;
   } catch (error) {
     console.log(error);
