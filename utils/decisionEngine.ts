f1112c02a6793c7c1674eb0da66c337c8319198c
itsyashsahu <itsyashsahu@gmail.com>
2023-09-03 08:54:10 +0530

    Fixed the url issue

--- utils/decisionEngine.ts
+++ utils/decisionEngine.ts
@@ -7,7 +7,7 @@ interface DecisionRequest {
 const fetchDecision = async (decisionRequest: DecisionRequest) => {
   try {
     const result = await axios.post(
-      `${process.env.PLATFORM_INTEGRATION_URL}/loan-approval-decision`,
+      `${process.env.PLATFORM_INTEGRATION_URL}/api/loan-approval-decision`,
       decisionRequest
     );
     return result.data;
