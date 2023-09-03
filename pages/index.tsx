8b8f378d7723974dd0f4e9c95fd8f7ca9f8ce77b
itsyashsahu <itsyashsahu@gmail.com>
2023-09-03 12:53:47 +0530

    Fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,13 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import { useSession, signIn, signOut } from "next-auth/react"
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const { data: session } = useSession()
-  console.log("🚀 ~ Home ~ session:", session)
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <button onClick={() => signIn()} >Hello </button>
+      <button>Hello From Backend-Layer</button>
     </main>
   )
 }
